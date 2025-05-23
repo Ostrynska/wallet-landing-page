c94e572c663791402539298708e1e8966de7d3e8
Ostrynska <kateriinag@gmail.com>
2025-05-24 00:49:48 +0300

    Update sprite.js

--- js/sprite.js
+++ js/sprite.js
@@ -1,28 +1,30 @@
 inlineExternalUse();
 
 function inlineExternalUse() {
-    let extSvgs = document.querySelectorAll('use');
-    if (extSvgs.length) {
-        extSvgs.forEach(function (item, i) {
-            let href = item.getAttribute('href')
-                ? item.getAttribute('href')
-                : item.getAttribute('xlink:href');
-            let hrefNew = '#' + href.split('#')[1];
-            item.setAttribute('href', hrefNew);
+    const extSvgs = document.querySelectorAll('use');
 
-            fetch(href)
-                .then(response => response.text())
-                .then(data => {
-                    let parser = new DOMParser();
-                    let svgInline = parser
-                        .parseFromString(data, 'application/xml')
-                        .querySelector('svg');
-                    svgInline.setAttribute('aria-hidden', 'true');
-                    svgInline.style.width = 0;
-                    svgInline.style.height = 0;
-                    svgInline.style.position = 'absolute';
-                    document.body.appendChild(svgInline);
-                });
-        });
-    }
+    extSvgs.forEach(useEl => {
+        if (!useEl.hasAttribute('aria-gradient')) return;
+
+        const href = useEl.getAttribute('href') || useEl.getAttribute('xlink:href');
+        if (!href || !href.includes('#')) return;
+
+        const [url, symbolId] = href.split('#');
+        const hrefNew = `#${symbolId}`;
+        useEl.setAttribute('href', hrefNew);
+
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                const parser = new DOMParser();
+                const svgInline = parser
+                    .parseFromString(data, 'image/svg+xml')
+                    .querySelector('svg');
+                svgInline.setAttribute('aria-hidden', 'true');
+                svgInline.style.width = 0;
+                svgInline.style.height = 0;
+                svgInline.style.position = 'absolute';
+                document.body.appendChild(svgInline);
+            });
+    });
 }
